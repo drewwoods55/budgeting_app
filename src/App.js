9bf8693c6dbeebb9a3f171501676cf883d9cc588
Drew Woods <drewwoods9@gmail.com>
2022-06-21 10:52:16 -0400

    configing new laptop to github

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <h1 className='me-auto'>Budgets</h1>
         <Button variant="primary">Add Budget</Button>
         <Button vasriant="outline-primary">Add Expense</Button>
+        
       </Stack>
       <div 
         style={{ 
